f525450a78742713bdc91cfc74e2c4c3d05673bd
TranVanHoa <vanhoa090875150@gmail.com>
2025-11-16 21:38:10 +0700

    refactor(user): rename username to userName in DTO for consistency
    style(prisma): format schema file and update client configuration

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -3,7 +3,7 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class CreateUserDto {
   @IsString()
   @IsNotEmpty()
-  username: string;
+  userName: string;
 
   @IsEmail()
   @IsNotEmpty()
